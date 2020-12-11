afd8649ff8fe5ab075475af87a3e5f7d40f6314c
Alex Felice <golfzander@comcast.net>
2020-12-11 13:34:17 -0600

    updated filename

--- static/js/logic.js
+++ static/js/logic.js
@@ -13,7 +13,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-pr
 });
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/majorAirports.json";
+let airportData = "https://raw.githubusercontent.com/XZandermarsh/Earthquakes/main/majorAirports.json";
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data)  {
